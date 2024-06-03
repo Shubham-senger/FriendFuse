985efb57985084ea536598f6b308058fd044c6b3
Mrinal <mrinalkandwal@gmail.com>
2024-06-03 12:25:40 +0530

    created post route for delete and creating post

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -8,7 +8,7 @@ const createPost = async (req, res) => {
         .status(400)
         .json({ message: "PotedBy and text fields are required." });
     }
-    const user =await User.findById(postedBy);
+    const user = await User.findById(postedBy);
     if (!user) {
       return res.status(404).json({ message: "User not found." });
     }
@@ -31,4 +31,33 @@ const createPost = async (req, res) => {
     console.log(error);
   }
 };
-export { createPost };
+
+const getPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      res.status(404).json({ message: "Post not found" });
+    }
+    res.status(200).json({ message: "Post found", post });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log(error);
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      res.status(404).json({ message: "Post not found" });
+    }
+    if (post.postedBy.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ message: "You can't delete this post" });
+    }
+    await Post.findByIdAndDelete(req.params.id);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log(error);
+  }
+};
+export { createPost, getPost, deletePost };
